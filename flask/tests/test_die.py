4cb249771b43b014628aa49e532d49f10fa6e851
Daniel Andersson <d.andersson86@icloud.com>
2022-01-23 20:18:50 +0100

    Add tests from terminal application to flask application.

---
+++ flask/tests/test_die.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+"""
+Test module for instances of class Die.
+"""
+import unittest
+import random
+from src.die import Die
+
+
+class TestDie(unittest.TestCase):
+    """
+    Submodule for unittests, derives from unittest.TestCase
+    """
+
+    def setUp(self):
+        """
+        Build up method that runs before each test case.
+        """
+        random.seed("ooPython_kmom01")
+        self.die = Die()
+
+
+    # def tearDown(self):
+    #     """
+    #     Tear down method that runs after each test case.
+    #     """
+
+
+    def test_initial_die_values(self):
+        """
+        Test the initial values on a new object instance of die.
+        """
+        self.assertEqual(self.die.get_value(), None)
+        self.assertNotEqual(self.die.get_value(), "")
+        self.assertNotEqual(self.die.get_value(), [])
+        self.assertNotEqual(self.die.get_value(), ())
+        self.assertEqual(self.die.sides, 6)
+        self.assertEqual(self.die.MIN_ROLL_VALUE, 1)
+        self.assertEqual(self.die.MAX_ROLL_VALUE, 6)
+
+        self.assertNotEqual(self.die.sides, "")
+
+        self.assertEqual(self.die.MIN_ROLL_VALUE, 1)
+        # Ska man kunna välja antal sidor på en tärning blir det här fel...
+        self.assertEqual(self.die.MAX_ROLL_VALUE, 6)
+
+
+    def test_die_attribute_types(self):
+        """
+        Test types on object instance attributes.
+        """
+        # self.assertEqual(type(self.die.get_value()), None)
+        self.assertNotEqual(type(self.die.get_value()), int)
+        self.assertNotEqual(type(self.die.get_value()), str)
+        self.assertNotEqual(type(self.die.get_value()), bool)
+        self.assertNotEqual(type(self.die.get_value()), list)
+        self.assertNotEqual(type(self.die.get_value()), dict)
+        self.assertNotEqual(type(self.die.get_value()), tuple)
+
+        self.assertEqual(type(self.die.sides), int)
+        self.assertNotEqual(type(self.die.sides), str)
+        self.assertNotEqual(type(self.die.sides), bool)
+        self.assertNotEqual(type(self.die.sides), list)
+        self.assertNotEqual(type(self.die.sides), dict)
+        self.assertNotEqual(type(self.die.sides), tuple)
+
+        self.assertEqual(type(self.die.__str__()), str)
+        self.assertNotEqual(type(self.die.__str__()), int)
+        self.assertNotEqual(type(self.die.__str__()), bool)
+        self.assertNotEqual(type(self.die.__str__()), list)
+        self.assertNotEqual(type(self.die.__str__()), dict)
+        self.assertNotEqual(type(self.die.__str__()), tuple)
+
+        self.assertEqual(type(self.die.get_name()), str)
+        self.assertNotEqual(type(self.die.get_name()), int)
+        self.assertNotEqual(type(self.die.get_name()), bool)
+        self.assertNotEqual(type(self.die.get_name()), list)
+        self.assertNotEqual(type(self.die.get_name()), dict)
+        self.assertNotEqual(type(self.die.get_name()), tuple)
+
+
+    def test_die_object_for_instance_of_class(self):
+        """
+        Test for the initial value
+        """
+        self.assertIsInstance(self.die, Die)
+
+
+    def test_die_roll_value_type(self):
+        """
+        Test die roll value type
+        """
+        self.assertEqual(type(self.die.roll()), int)
+        self.assertNotEqual(type(self.die.roll()), str)
+        self.assertNotEqual(type(self.die.roll()), bool)
+        self.assertNotEqual(type(self.die.roll()), list)
+        self.assertNotEqual(type(self.die.roll()), dict)
+        self.assertNotEqual(type(self.die.roll()), tuple)
+
+
+    def test_random_dice_roll(self):
+        """
+        Test die roll for random value with random seed
+        """
+        self.assertEqual(self.die.roll(), 2)
---
+++ flask/tests/test_hand.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+"""
+Test module for instances of class Hand.
+"""
+import unittest
+from src.hand import Hand
+from src.die import Die
+
+
+class TestHand(unittest.TestCase):
+    """
+    Submodule for unittests, derives from unittest.TestCase
+    """
+
+    def setUp(self):
+        """
+        Build up method that runs before each test case.
+        """
+        self.hand = Hand()
+
+
+    # def tearDown(self):
+    #     """
+    #     Tear down method that runs after each test case.
+    #     """
+
+
+    def test_initial_hand_values(self):
+        """
+        Test the initial values on a new object instance of die.
+        """
+        self.assertEqual(self.hand.get_total_value(), 0)
+        self.assertEqual(self.hand.__str__(), "0,0,0,0,0")
+
+
+    def test_hand_attribute_types(self):
+        """
+        Test types on object instance attributes.
+        """
+        self.assertEqual(type(self.hand.get_total_value()), int)
+        self.assertNotEqual(type(self.hand.get_total_value()), str)
+        self.assertNotEqual(type(self.hand.get_total_value()), bool)
+        self.assertNotEqual(type(self.hand.get_total_value()), list)
+        self.assertNotEqual(type(self.hand.get_total_value()), dict)
+        self.assertNotEqual(type(self.hand.get_total_value()), tuple)
+
+        self.assertEqual(type(self.hand.__str__()), str)
+        self.assertNotEqual(type(self.hand.__str__()), int)
+        self.assertNotEqual(type(self.hand.__str__()), bool)
+        self.assertNotEqual(type(self.hand.__str__()), list)
+        self.assertNotEqual(type(self.hand.__str__()), dict)
+        self.assertNotEqual(type(self.hand.__str__()), tuple)
+
+        self.assertEqual(type(self.hand.indexes), list)
+        self.assertNotEqual(type(self.hand.indexes), str)
+        self.assertNotEqual(type(self.hand.indexes), bool)
+        self.assertNotEqual(type(self.hand.indexes), int)
+        self.assertNotEqual(type(self.hand.indexes), dict)
+        self.assertNotEqual(type(self.hand.indexes), tuple)
+
+        self.assertEqual(type(self.hand.die), list)
+        self.assertNotEqual(type(self.hand.die), str)
+        self.assertNotEqual(type(self.hand.die), bool)
+        self.assertNotEqual(type(self.hand.die), int)
+        self.assertNotEqual(type(self.hand.die), dict)
+        self.assertNotEqual(type(self.hand.die), tuple)
+
+        self.assertEqual(type(self.hand.get_values()), list)
+        self.assertNotEqual(type(self.hand.get_values()), str)
+        self.assertNotEqual(type(self.hand.get_values()), bool)
+        self.assertNotEqual(type(self.hand.get_values()), int)
+        self.assertNotEqual(type(self.hand.get_values()), dict)
+        self.assertNotEqual(type(self.hand.get_values()), tuple)
+
+
+    def test_hand_object_for_instance_of_class(self):
+        """
+        Test hand for object instances.
+        """
+        self.assertIsInstance(self.hand, Hand)
+        self.assertNotIsInstance(self.hand, Die)
+
+
+    def test_die_objects_in_hand_for_instance_of_class(self):
+        """
+        Test dice hand dices for object instances.
+        """
+        for die in self.hand.die:
+            self.assertIsInstance(die, Die)
+            self.assertNotIsInstance(die, Hand)
+
+
+    def test_hand_roll_value_type(self):
+        """
+        Test die roll value type
+        """
+        self.assertEqual(type(self.hand.roll()), list)
+        self.assertNotEqual(type(self.hand.roll()), str)
+        self.assertNotEqual(type(self.hand.roll()), int)
+        self.assertNotEqual(type(self.hand.roll()), bool)
+        self.assertNotEqual(type(self.hand.roll()), dict)
+        self.assertNotEqual(type(self.hand.roll()), tuple)
